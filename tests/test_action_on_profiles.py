58a35479b6005372ed6da43d6a6cb6611c54f093
Jacek Szambelańczyk <jacekszambelanczyk@gmail.com>
2025-09-20 00:03:26 +0200

    Adding new tests

--- tests/test_action_on_profiles.py
+++ tests/test_action_on_profiles.py
@@ -14,6 +14,16 @@ def mock_environment(monkeypatch, tmp_path):
     return users_file
 
 
+def assert_profiles(result, username, password, users_file):
+    assert result["username"] == username
+    assert result["password"] == password
+    assert Path(result["file"]).exists()
+
+    with open(users_file, "r") as f:
+        data = json.load(f)
+    assert result in data
+
+
 @pytest.mark.parametrize("username, password", [
     ("jacek", "123"),
     ("zxddfajshflkmfasdf", "fdsjkh13AFDJAKF!#!#!"),
@@ -25,11 +35,57 @@ def test_create_profile_everything_correct(mock_environment, monkeypatch, userna
     inputs = [username, password, password]
     monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
     result = create_profile(users_file)
+    assert_profiles(result, username, password, users_file)
 
-    assert result["username"] == username
-    assert result["password"] == password
-    assert Path(result["file"]).exists()
 
+@pytest.mark.parametrize("username1, password, username2", [
+    ("jacek", "123", "jabzek"),
+    ("John321", "!DSgdfkmap", "John12345!")])
+
+
+def test_create_profile_user_exists(mock_environment, monkeypatch, username1, password, username2, tmp_path):
+    users_file = mock_environment
+    inputs = [username1, username2, password, password]
+    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
+    Path.touch(tmp_path / f"{username1}.json")
+    result = create_profile(users_file)
+    assert_profiles(result, username2, password, users_file)
+
+
+@pytest.mark.parametrize("username, password1, password2, password", [
+    ("jacek", "123", "321", "123"),
+    ("William654$", "gkap3vmASB", "Gkap3vmASB", "h2VA!#FDDDA")])
+
+
+def test_create_profile_different_passwords(mock_environment, monkeypatch, username, password1, password2, password):
+    users_file = mock_environment
+    inputs = [username, password1, password2, password, password]
+    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
+    result = create_profile(users_file)
+    assert_profiles(result, username, password, users_file)
+
+
+@pytest.mark.parametrize("username, password", [
+    ("jacek", "123"),
+    ("zxddfajshflkmfasdf", "fdsjkh13AFDJAKF!#!#!"),
+    ("Magda123", "d3D!gdfnXPor")])
+
+
+def test_create_profile_second_user(mock_environment, monkeypatch, username, password, tmp_path):
+    users_file = mock_environment
+    inputs = [username, password, password]
+    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
+    users_data = {"username": "Tomato5214",
+                  "password": "Potato9185",
+                  "file": f"{tmp_path} / Tomato5214.json"}
+    
+    with open(users_file, "w") as f:
+        json.dump([users_data], f)
+    result = create_profile(users_file)
+    
     with open(users_file, "r") as f:
         data = json.load(f)
-    assert data == [result]
+    
+    assert len(data) == 2
+    assert data[0] == users_data
+    assert data[1] == result
