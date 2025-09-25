dd3a94ead284dbdd31a46f2dc9992b3f2334e21e
Jacek Szambelańczyk <jacekszambelanczyk@gmail.com>
2025-09-25 23:01:40 +0200

    Adding get_date function

--- task_manager/action_on_tasks.py
+++ task_manager/action_on_tasks.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, asdict
 from datetime import datetime
-from dateutil.relativedelta import relativedelta 
+
 
 
 @dataclass
@@ -14,5 +14,60 @@ class Task:
     date_of_start: str
 
 
-def get_date():
-    pass
+def get_date(current_time):
+    current_year = current_time.year
+    current_month = current_time.month
+    current_day = current_time.day
+    current_hour = current_time.hour
+    current_minute = current_time.minute
+
+    while True:
+        try:
+            year = int(input("Provide year: "))
+            if year < current_year:
+                raise ValueError
+            break
+        except ValueError:
+            print("Provide correct year. Try again.")
+
+    while True:
+        try:
+            month = int(input("Provide month: "))
+            temp_date = datetime(year, month, day=1)
+            if temp_date < datetime(current_year, current_month, day=1):
+                raise ValueError
+            break
+        except ValueError:
+            print("Provide correct month. Try again.")
+
+    while True:
+        try:
+            day = int(input("Provide day: "))
+            temp_date = datetime(year, month, day)
+            if temp_date < datetime(current_year, current_month, current_day):
+                raise ValueError
+            break
+        except ValueError:
+            print("Provide correct day. Try again.")
+
+    while True:
+        try:
+            hour = int(input("Provide hour: "))
+            temp_date = datetime(year, month, day, hour)
+            if temp_date < datetime(current_year, current_month, current_day, current_hour):
+                raise ValueError
+            break
+        except ValueError:
+            print("Provide correct hour. Try again.")
+
+    while True:
+        try:
+            minute = int(input("Provide minute: "))
+            temp_date = datetime(year, month, day, hour, minute)
+            if temp_date <= datetime(current_year, current_month, current_day, current_hour, current_minute):
+                raise ValueError
+            break
+        except ValueError:
+            print("Provide correct minute. Try again.")
+
+    return temp_date
