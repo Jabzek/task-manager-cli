76bfffeeb6687a2a827a28ccf9c960773c4989d4
Jacek Szambelańczyk <jacekszambelanczyk@gmail.com>
2025-09-25 14:38:12 +0200

    Adding file with Task class and get_date function

--- task_manager/action_on_tasks.py
+++ task_manager/action_on_tasks.py
@@ -1,1 +1,18 @@
-pass
+from dataclasses import dataclass, asdict
+from datetime import datetime
+from dateutil.relativedelta import relativedelta 
+
+
+@dataclass
+class Task:
+    name: str
+    description: str
+    priority: str    #urgent, important, not important
+    date_of_creation: str
+    deadline: str
+    status: str     #active, pending 
+    date_of_start: str
+
+
+def get_date():
+    pass
