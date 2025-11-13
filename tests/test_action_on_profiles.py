e2526eed1fe2df2b2bbf1de2bf96584e4818cc9a
Jacek Szambelańczyk <jacekszambelanczyk@gmail.com>
2025-11-13 23:08:32 +0100

    Removing unnecessary tests
    I removed tests for data input functions because method of getting data
    will be changed

--- tests/test_action_on_profiles.py
+++ tests/test_action_on_profiles.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 from task_manager import action_on_profiles
-from task_manager.action_on_profiles import create_profile, log_into_profile, delete_profile, change_password
+from task_manager.action_on_profiles import saving_new_profile, log_into_profile, del_profile, saving_new_password
 
 @pytest.fixture
 def mock_environment(monkeypatch, tmp_path):
@@ -12,6 +12,29 @@ def mock_environment(monkeypatch, tmp_path):
     monkeypatch.setattr(action_on_profiles, "get_file", lambda name: tmp_path / f"{name}.json")
     users_file = tmp_path / "users.json"
     return users_file
+
+
+@pytest.mark.parametrize("username, password", [
+    ("jacek", "123"),
+    ("Kobi!!!", "shdgfhbsf")
+])
+
+
+def test_save_new_profile(mock_environment, tmp_path, username, password):
+    users_file = mock_environment
+    user_file = tmp_path / (username + ".json")
+    user_data = {"username": username,
+                 "password": password,
+                 "file": str(user_file)}
+    test_user_data = saving_new_profile(users_file, user_file, username, password)
+
+    assert user_data == test_user_data
+    assert user_file.exists()
+
+    with open(users_file, "r") as f:
+        data = json.load(f)    
+
+    assert user_data in data
 
 
 def saving_data_to_file(users_file, tmp_path):
@@ -26,83 +49,6 @@ def saving_data_to_file(users_file, tmp_path):
 
     for file in file_list:
         Path(file).touch()
-
-
-def assert_profiles_creation(result, username, password, users_file):
-    assert result["username"] == username
-    assert result["password"] == password
-    assert Path(result["file"]).exists()
-
-    with open(users_file, "r") as f:
-        data = json.load(f)
-    assert result in data
-
-
-@pytest.mark.parametrize("username, password", [
-    ("jacek", "123"),
-    ("zxddfajshflkmfasdf", "fdsjkh13AFDJAKF!#!#!"),
-    ("Magda123", "d3D!gdfnXPor")])
-
-
-def test_create_profile_everything_correct(mock_environment, monkeypatch, username, password):
-    users_file = mock_environment
-    inputs = [username, password, password]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    result = create_profile(users_file)
-    assert_profiles_creation(result, username, password, users_file)
-
-
-@pytest.mark.parametrize("username1, password, username2", [
-    ("jacek", "123", "jabzek"),
-    ("John321", "!DSgdfkmap", "John12345!")])
-
-
-def test_create_profile_user_exists(mock_environment, monkeypatch, username1, password, username2, tmp_path):
-    users_file = mock_environment
-    inputs = [username1, username2, password, password]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    (tmp_path / f"{username1}.json").touch()
-    result = create_profile(users_file)
-    assert_profiles_creation(result, username2, password, users_file)
-
-
-@pytest.mark.parametrize("username, password1, password2, password", [
-    ("jacek", "123", "321", "123"),
-    ("William654$", "gkap3vmASB", "Gkap3vmASB", "h2VA!#FDDDA")])
-
-
-def test_create_profile_different_passwords(mock_environment, monkeypatch, username, password1, password2, password):
-    users_file = mock_environment
-    inputs = [username, password1, password2, password, password]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    result = create_profile(users_file)
-    assert_profiles_creation(result, username, password, users_file)
-
-
-@pytest.mark.parametrize("username, password", [
-    ("jacek", "123"),
-    ("zxddfajshflkmfasdf", "fdsjkh13AFDJAKF!#!#!"),
-    ("Magda123", "d3D!gdfnXPor")])
-
-
-def test_create_profile_second_user(mock_environment, monkeypatch, username, password, tmp_path):
-    users_file = mock_environment
-    inputs = [username, password, password]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    users_data = {"username": "Tomato5214",
-                  "password": "Potato9185",
-                  "file": f"{tmp_path} / Tomato5214.json"}
-    
-    with open(users_file, "w") as f:
-        json.dump([users_data], f)
-    result = create_profile(users_file)
-    
-    with open(users_file, "r") as f:
-        data = json.load(f)
-    
-    assert len(data) == 2
-    assert data[0] == users_data
-    assert data[1] == result
 
 
 def assert_profiles_login(username, password, result, tmp_path):
@@ -154,183 +100,44 @@ def test_log_into_profile_wrong_password(mock_environment, monkeypatch, username
     assert_profiles_login(username, password2, result, tmp_path)
 
 
-def assert_profiles_deleting(user_data, user_file, users_file):
+@pytest.mark.parametrize("user_data", [
+    {"username": "jacek", "password": "123"},
+    {"username": "leon4jds", "password": "F@BafvLOP"}
+])
+
+
+def test_delete_profile(mock_environment, tmp_path, user_data):
+    users_file = mock_environment
+    file = tmp_path / (user_data["username"] + ".json")
+    user_data["file"] = str(file)
+    saving_data_to_file(users_file, tmp_path)
+    del_profile(users_file, user_data)
+
+    assert not file.exists()
+
     with open(users_file, "r") as f:
-        users_data = json.load(f)
-    
-    assert user_data not in users_data
-    assert not user_file.exists()
+        data = json.load(f)
+
+    assert user_data not in data
 
 
-@pytest.mark.parametrize("username, password, decision", [
-    ("jacek", "123", "yes"),
-    ("leon4jds", "F@BafvLOP", "yes"),
-    ("ann531", "hobbit123", "yes"),
-    ("johnus941", "arZWksPP", "yes")])
+@pytest.mark.parametrize("username, new_password", [
+    ("jacek", "321"),
+    ("ann531", "GRAc1n")
+])
 
 
-def test_delete_profile_everything_correct(mock_environment, monkeypatch, tmp_path, username, password, decision):
+def test_change_password(mock_environment, tmp_path, username, new_password):
     users_file = mock_environment
+    new_user_data = {"username": username, "password": new_password, "file": str(tmp_path / (username + ".json"))}
     saving_data_to_file(users_file, tmp_path)
-    user_file = tmp_path / f"{username}.json"
-    user_data = {"username": username, "password": password, "file": str(user_file)}
-    inputs = [password, decision, decision]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    delete_profile(user_data, users_file)
-    assert_profiles_deleting(user_data, user_file, users_file)
+    test_user_data = saving_new_password(users_file, username, new_password)
 
+    assert test_user_data == new_user_data
 
-@pytest.mark.parametrize("username, password1, password2, decision", [
-    ("jacek", "321", "123", "yes"),
-    ("leon4jds", "Fsckj123lv", "F@BafvLOP", "yes"),
-    ("ann531", "vader123", "hobbit123", "yes"),
-    ("johnus941", "njzpjnDGK", "arZWksPP", "yes")])
-
-
-def test_delete_profile_wrong_password(mock_environment, monkeypatch, tmp_path, username, password1, password2, decision):
-    users_file = mock_environment
-    saving_data_to_file(users_file, tmp_path)
-    user_file = tmp_path / f"{username}.json"
-    user_data = {"username": username, "password": password2, "file": str(user_file)}
-    inputs = [password1, password2, decision, decision]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    delete_profile(user_data, users_file)
-    assert_profiles_deleting(user_data, user_file, users_file)
-
-
-def assert_profiles_deleting_cancel(user_data, user_file, users_file):
     with open(users_file, "r") as f:
-        users_data = json.load(f)
-    
-    assert user_data in users_data
-    assert user_file.exists()
+        data = json.load(f)
 
-
-@pytest.mark.parametrize("username, password, decision", [
-    ("jacek", "123", "return"),
-    ("leon4jds", "F@BafvLOP", "return"),
-    ("ann531", "hobbit123", "return"),
-    ("johnus941", "arZWksPP", "return")])
-
-
-def test_delete_profile_cancel1(mock_environment, monkeypatch, tmp_path, username, password, decision):
-    users_file = mock_environment
-    saving_data_to_file(users_file, tmp_path)
-    user_file = tmp_path / f"{username}.json"
-    user_data = {"username": username, "password": password, "file": str(user_file)}
-    inputs = [decision]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    delete_profile(user_data, users_file)
-    assert_profiles_deleting_cancel(user_data, user_file, users_file)
-
-
-@pytest.mark.parametrize("username, password, decision", [
-    ("jacek", "123", "no"),
-    ("leon4jds", "F@BafvLOP", "no"),
-    ("ann531", "hobbit123", "no"),
-    ("johnus941", "arZWksPP", "no")])
-
-
-def test_delete_profile_cancel2(mock_environment, monkeypatch, tmp_path, username, password, decision):
-    users_file = mock_environment
-    saving_data_to_file(users_file, tmp_path)
-    user_file = tmp_path / f"{username}.json"
-    user_data = {"username": username, "password": password, "file": str(user_file)}
-    inputs = [password, decision]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    delete_profile(user_data, users_file)
-    assert_profiles_deleting_cancel(user_data, user_file, users_file)
-
-
-def assert_change_password(users_file, username, old_password, new_password, cancel):
-    with open(users_file, "r") as f:
-        users_data = json.load(f)
-    
-    for user in users_data:
-        if user["username"] == username:
-            if not cancel:
-                assert user["password"] == new_password
-            else:
-                assert user["password"] == old_password
-            break
-
-
-@pytest.mark.parametrize("username, password1, password2", [
-    ("jacek", "123", "321"),
-    ("leon4jds", "F@BafvLOP", "SBakkds"),
-    ("ann531", "hobbit123", "vader123"),
-    ("johnus941", "arZWksPP", "Blaju3")])
-
-
-def test_change_password_everything_correct(mock_environment, monkeypatch, tmp_path, username, password1, password2):
-    users_file = mock_environment
-    saving_data_to_file(users_file, tmp_path)
-    inputs = [password1, password2, password2]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    change_password(users_file, username, password1)
-    assert_change_password(users_file, username, password1, password2, cancel=False)
-
-
-@pytest.mark.parametrize("username, wrong_password, password1, password2", [
-    ("jacek", "fnjs", "123", "321"),
-    ("leon4jds", "SJFj13gd", "F@BafvLOP", "SBakkds"),
-    ("ann531", "GDjahhpi32", "hobbit123", "vader123"),
-    ("johnus941", "mkmsafk@#%$", "arZWksPP", "Blaju3")])
-
-
-def test_change_password_wrong_old_password(mock_environment, monkeypatch, tmp_path, username, wrong_password, password1, password2):
-    users_file = mock_environment
-    saving_data_to_file(users_file, tmp_path)
-    inputs = [wrong_password, password1, password2, password2]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    change_password(users_file, username, password1)
-    assert_change_password(users_file, username, password1, password2, cancel=False)
-
-
-@pytest.mark.parametrize("username, password1, password2", [
-    ("jacek", "123", "321"),
-    ("leon4jds", "F@BafvLOP", "SBakkds"),
-    ("ann531", "hobbit123", "vader123"),
-    ("johnus941", "arZWksPP", "Blaju3")])
-
-
-def test_change_password_same_password(mock_environment, monkeypatch, tmp_path, username, password1, password2):
-    users_file = mock_environment
-    saving_data_to_file(users_file, tmp_path)
-    inputs = [password1, password1, password2, password2]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    change_password(users_file, username, password1)
-    assert_change_password(users_file, username, password1, password2, cancel=False)
-
-
-@pytest.mark.parametrize("username, wrong_password, password1, password2", [
-    ("jacek", "fnjs", "123", "321"),
-    ("leon4jds", "SJFj13gd", "F@BafvLOP", "SBakkds"),
-    ("ann531", "GDjahhpi32", "hobbit123", "vader123"),
-    ("johnus941", "mkmsafk@#%$", "arZWksPP", "Blaju3")])
-
-
-def test_change_password_bad_repetition(mock_environment, monkeypatch, tmp_path, username, wrong_password, password1, password2):
-    users_file = mock_environment
-    saving_data_to_file(users_file, tmp_path)
-    inputs = [password1, password2, wrong_password, password2, password2]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    change_password(users_file, username, password1)
-    assert_change_password(users_file, username, password1, password2, cancel=False)
-
-
-@pytest.mark.parametrize("username, password, decision", [
-    ("jacek", "123", "return"),
-    ("leon4jds", "F@BafvLOP", "return"),
-    ("ann531", "hobbit123", "return"),
-    ("johnus941", "arZWksPP", "return")])
-
-
-def test_change_password_cancel(mock_environment, monkeypatch, tmp_path, username, password, decision):
-    users_file = mock_environment
-    saving_data_to_file(users_file, tmp_path)
-    inputs = [decision]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    change_password(users_file, username, password)
-    assert_change_password(users_file, username, password, None, cancel=True)
-
+    for el in data:
+        if username == el["username"]:
+            assert new_password == el["password"]
--- tests/test_action_on_tasks.py
+++ tests/test_action_on_tasks.py
@@ -5,11 +5,11 @@ import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 from task_manager import action_on_tasks
-from task_manager.action_on_tasks import get_date, create_task
+from task_manager.action_on_tasks import get_date, create_task, Task, create_task_input
 
 @pytest.fixture
 def get_date_fixture(monkeypatch):
-    monkeypatch.setattr("builtins.print", lambda *a, **kw: None)
+    monkeypatch.setattr("builtins.print", lambda *a, **kw: None) # args - argumenty pozycyjne(*), kwargs(**) - argumenty nazwane
     fake_now = datetime(2025, 5, 14, 12, 30)
     return fake_now
 
@@ -77,76 +77,21 @@ def create_task_fixture(monkeypatch, tmp_path):
     return task_date_creation, task_date_creation_str, user_file
 
 
-def saving_date(user_file, task_date_creation):
-    data = {"pending": [{"name": "task1", "description": "description1", "priority": "urgent",
-                "date_of_creation": task_date_creation, "deadline": datetime(2025, 6, 10, 18, 0).strftime("%Y-%m-%d %H:%M")},
-                {"name": "name_taken", "description": "description2", "priority": "important",
-                "date_of_creation": task_date_creation, "deadline": datetime(2025, 9, 11, 18, 0).strftime("%Y-%m-%d %H:%M")}],
-            "active": [{"name": "task2", "description": "description3", "priority": "urgent",
-                "date_of_creation": task_date_creation, "deadline": datetime(2026, 2, 1, 15, 0).strftime("%Y-%m-%d %H:%M")}],
-            "finished": []}
-    with open(user_file, "w") as f:
-        json.dump(data, f, indent=2)
+@pytest.mark.parametrize("name, description, priority, status, deadline", [
+    ("task1", "", "important", "active", datetime(2025, 6, 10, 18, 0).strftime("%Y-%m-%d %H:%M")),
+    ("task2", "task2 description", "urgent", "pending", datetime(2025, 9, 11, 18, 0).strftime("%Y-%m-%d %H:%M"))
+])
 
 
-def assert_task_create(user_file, task, active_status):
+def test_create_task(create_task_fixture, monkeypatch, name, description, priority, status, deadline):
+    task_date_creation, task_date_creation_str, user_file = create_task_fixture
+    test_task = Task(name, description, priority, status, task_date_creation_str, deadline)
+    test_task_dic = test_task.to_dict()
+    monkeypatch.setattr(action_on_tasks, "create_task_input", 
+                        lambda *_: (name, description, priority, deadline, status))
+    create_task(task_date_creation, user_file)
+
     with open(user_file, "r") as f:
         tasks = json.load(f)
-    
-    if active_status == "yes":
-        assert task in tasks["active"]
-    else:
-        assert task in tasks["pending"]
 
-
-@pytest.mark.parametrize("name, description, priority, deadline, active_status", (
-    ("task1", "description1", "urgent", "2025-6-10 18:00", "no"),
-    ("task2", "descripiton2", "important", "2025-9-11 15:00", "yes"),
-    ("task3", "", "not important", "2026-3-17 19:00", "no")))
-
-
-def test_create_task_adding_first_task(monkeypatch, create_task_fixture, name, description, priority, deadline, active_status):
-    task_date_creation, task_date_creation_str, user_file = create_task_fixture
-    monkeypatch.setattr(action_on_tasks, "get_date", lambda _: deadline)
-    inputs = [name, description, priority, active_status]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    task = {"name": name, "description": description, "priority": priority,
-            "date_of_creation": task_date_creation_str, "deadline": deadline}
-    create_task(task_date_creation, user_file)
-    assert_task_create(user_file, task, active_status)
-
-
-@pytest.mark.parametrize("wrong_name, name, description, wrong_priority, priority, deadline, wrong_status, active_status", (
-    ("a" * 30, "task", "description1", "good", "urgent", "2025-6-10 18:00", "status", "no"),
-    ("", "1", "descripiton2", "bad", "important", "2025-9-11 15:00", "jafdsnfj", "no"),
-    ("name_taken", "1", "descripiton3", "insteresting", "important", "2025-9-11 15:00", "active", "yes")))
-
-
-def test_create_task_wrong_data(monkeypatch, create_task_fixture, wrong_name, name, description, wrong_priority, priority, deadline, wrong_status, active_status):
-    task_date_creation, task_date_creation_str, user_file = create_task_fixture
-    monkeypatch.setattr(action_on_tasks, "get_date", lambda _: deadline)
-    inputs = [wrong_name, name, description, wrong_priority, priority, wrong_status, active_status]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    task = {"name": name, "description": description, "priority": priority,
-            "date_of_creation": task_date_creation_str, "deadline": deadline}
-    saving_date(user_file, task_date_creation_str)
-    create_task(task_date_creation, user_file)
-    assert_task_create(user_file, task, active_status)
-
-
-@pytest.mark.parametrize("name, description, priority, deadline, active_status", (
-    ("task4", "description1", "urgent", "2025-5-20 18:00", "yes"),
-    ("task5", "descripiton2", "important", "2025-9-11 15:00", "no"),
-    ("task6", "", "not important", "2026-3-17 19:00", "no")))
-
-
-def test_create_task_adding_another_task(monkeypatch, create_task_fixture, name, description, priority, deadline, active_status):
-    task_date_creation, task_date_creation_str, user_file = create_task_fixture
-    monkeypatch.setattr(action_on_tasks, "get_date", lambda _: deadline)   
-    inputs = [name, description, priority, active_status]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
-    task = {"name": name, "description": description, "priority": priority,
-            "date_of_creation": task_date_creation_str, "deadline": deadline}
-    saving_date(user_file, task_date_creation_str)
-    create_task(task_date_creation, user_file)
-    assert_task_create(user_file, task, active_status)
+    assert tasks[status][0] == test_task_dic
